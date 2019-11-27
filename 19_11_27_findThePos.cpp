4a3e83434d5d10f570b35bbbca6f470ca1665c7f
Sheena爽 <18365368008@163.com>
2019-11-27 17:25:07 +0800

    施爽：于2019年11月27日上传的每日两题的第十七天的第一题的代码。(vector<vector<int>> vv(n, vector<int>(m, 0)))。

---
+++ 19_11_27_findThePos.cpp
@@ -0,0 +1,211 @@
+/*
+链接：https://www.nowcoder.com/practice/8ef655edf42d4e08b44be4d777edbf43?tpId=37&&tqId=21276&rp=1&ru=/activity/oj&qru=/ta/huawei/question-ranking
+来源：牛客网
+题目描述
+            1
+         1  1  1
+      1  2  3  2  1
+   1  3  6  7  6  3  1
+1  4  10 16 19  16 10  4  1
+以上三角形的数阵，第一行只有一个数1，以下每行的每个数，是恰好是它上面的数，左上角数到右上角的数，3个数之和（如果不存在某个数，认为该数就是0）。
+求第n行第一个偶数出现的位置。如果没有偶数，则输出-1。例如输入3,则输出2，输入4则输出3。
+输入n(n <= 1000000000)
+输入描述:
+输入一个int整数
+输出描述:
+输出返回的int值
+示例1
+输入
+4
+输出
+3
+*/
+#include <iostream>
+
+using namespace std;
+
+int count_the_num(int n)
+{
+    if(n < 3)
+        return -1;
+    if(n % 4 == 0)
+        return 3;
+    else if(n % 4 == 2)
+        return 4;
+    else 
+        return 2;
+}
+int main()
+{
+    int n = 0;
+    while(cin >> n)
+    {
+        int sum = 0;
+        sum = count_the_num(n);
+        cout << sum << endl;
+    }
+    
+    return 0;
+}
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    
+    while(cin >> n)
+    {
+        int m = 2*n - 1;
+        vector<vector<int>> vv(n, vector<int>(m, 0));
+        vv[0][0] = 1;
+        for(int i = 1; i < n; ++i)
+        {
+            vv[i][0] = vv[i][2 * i] = 1;
+            for(int j = 1; j < 2 * i; ++j)
+            {
+                if(1 == j)
+                    vv[i][j] = vv[i - 1][j] + vv[i - 1][j - 1];
+                else
+                    vv[i][j] = vv[i - 1][j - 2] + vv[i - 1][j - 1] + vv[i - 1][j];
+            }
+        }
+        
+        int count = 0;
+        int j = 0;
+        for(j = 0; j < 2 * n - 1; ++j)
+        {
+            if(vv[n - 1][j] % 2 == 0 && vv[n - 1][j] != 0)
+            {
+                count = j + 1;
+                break;
+            }
+        }
+        if(j == (2 * n - 1))
+            count = -1;
+        
+        cout << count << endl;
+    }
+    
+    return 0;
+}
+
+
+/*
+链接：https://www.nowcoder.com/practice/5821836e0ec140c1aa29510fd05f45fc?tpId=37&&tqId=21301&rp=1&ru=/activity/oj&qru=/ta/huawei/question-ranking
+来源：牛客网
+题目描述
+请设计一个算法完成两个超长正整数的加法。
+输入描述:
+输入两个字符串数字
+输出描述:
+输出相加后的结果，string型
+示例1
+输入
+99999999999999999999999999999999999999999999999999
+1
+输出
+100000000000000000000000000000000000000000000000000
+*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string theLongInteger(string s1, string s2)
+{
+    int i = 0;
+    int len = s1.size();
+    if(len < s2.size())
+        len = s2.size();
+    int ret = 0;
+    string res;
+    while(i < len)
+    {
+        int a = 0, b = 0;
+        if(i < s1.size())
+            a = s1[i] - '0';
+        else
+            a = 0;
+        if(i < s2.size())
+            b = s2[i] - '0';
+        else
+            b = 0;
+        int sum = ret + a + b;
+        ret = sum / 10;
+        sum = sum % 10;
+        res += (sum + '0');
+        ++i;
+    }
+    if(ret == 1)
+        res += '1';
+    reverse(res.begin(), res.end());
+    
+    return res;
+}
+int main()
+{
+    string s1, s2;
+    
+    while(cin >> s1 >> s2)
+    {
+        reverse(s1.begin(), s1.end());
+        reverse(s2.begin(), s2.end());
+        string res = theLongInteger(s1, s2);
+        
+        cout << res << endl;
+    }
+    
+    return 0;
+}
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string theLongInteger(string s1, string s2)
+{
+    int i = 0;
+    int len = s1.size();
+    if(len < s2.size())
+        len = s2.size();
+    int ret = 0;
+    string res;
+    while(i < len)
+    {
+        int a = 0, b = 0;
+        a = (i < s1.size()) ? (s1[i] - '0') : 0;
+        b = (i < s2.size()) ? (s2[i] - '0') : 0;
+        int sum = ret + a + b;
+        ret = sum / 10;
+        res += (sum % 10 + '0');
+        ++i;
+    }
+    if(ret == 1)
+        res += '1';
+    reverse(res.begin(), res.end());
+    
+    return res;
+}
+int main()
+{
+    string s1, s2;
+    
+    while(cin >> s1 >> s2)
+    {
+        reverse(s1.begin(), s1.end());
+        reverse(s2.begin(), s2.end());
+        string res = theLongInteger(s1, s2);
+        
+        cout << res << endl;
+    }
+    
+    return 0;
+}
